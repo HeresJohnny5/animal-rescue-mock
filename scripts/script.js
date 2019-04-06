2acd620c4764b839d9815302ce88421d41bc8092
HeresJohnny5 <ericksonjohn01@hotmail.com>
2019-04-06 11:23:26 -0400

    Add typedjs library. Add typedjs to make-a-difference section.

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+/* TYPEDJS SCRIPT */
+var options = {
+  backDelay: 500,
+  backSpeed: 60,
+  cursorChar: "|",
+  strings: ["Adopt ^1000", "Donate ^1000", "Volunteer ^1000"],
+  startDelay: 600,
+  stringsElement: null,
+  typeSpeed: 60,
+  loop: true
+}
+
+var typed = new Typed(".typedjs-strings", options);
